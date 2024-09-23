7efc405e0a7ed7ff859907d0b9ac7dbd76b714e0
Dripgod FTW <56833879+aryanaherofficial@users.noreply.github.com>
2024-09-23 13:15:19 +0530

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,211 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I focus on client collaboration and quality ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "Constantly trying to upgrade",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development and adapting ongoing trend changes.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Finished a Modern SaaS Landing Page using ReactJS",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Machine Learning Trading Bot Using ANN",
+    des: "Use ML Algos to predict market movements based on Nearest Neighbour algorithms",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "ReactJS Landing Page ",
+    des: "ReactJS based SaaS Mobile Friendly Dynamic Landing Page using TailwindCSS",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "BlockChain E-Voting System",
+    des: "Solidity based BlockChain Voting application to ensure fair and regulated process",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Blockchain based Attendance System",
+    des: "Solidity based BlockChain Attendance System to ensure fair attendance",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Aryan on our ReactJS SaaS landing page was a great experience. His expertise in front-end development and design brought our vision to life with impressive attention to detail. Aryan's ability to balance creativity with functionality resulted in a seamless, engaging user experience. If you're looking to create a responsive and dynamic landing page that truly stands out, Aryan’s your go-to",
+    name: "Ojas Jinde",
+    title: "Fiverr",
+  },
+  {
+    quote:
+      "Working with Aryan on our ML trading indicator was an outstanding experience. His expertise in machine learning, coupled with his commitment to precision and performance, made a significant difference throughout the project. Aryan’s innovative approach and passion for trading technology are truly remarkable. If you're looking to enhance your trading strategies with cutting-edge ML solutions, Aryan is the ideal collaborator.",
+    name: "Sameshwari Reddy",
+    title: "Upwork",
+  },
+  {
+    quote:
+      "Collaborating with Aryan on our blockchain project was an exceptional experience. His deep knowledge of decentralized technologies, proactive approach, and attention to detail were evident at every stage. Adrian's passion for innovation and problem-solving truly shines through. If you're looking to drive your blockchain initiatives forward and create lasting impact, Aryan is the perfect partner to bring your vision to life.",
+    name: "Abhishek Sherikar",
+    title: "MET INSTITUTE OF ENGINEERING",
+  },
+  {
+    quote:
+      "Collaborating with Aryan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aryan is the ideal partner.",
+    name: "Aadesh Shetty",
+    title: "Upwork",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Freelance Web Development",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Machine Learning Trading Indicator",
+    desc: "Developed a ML Algo based Trading Indicator to predict future Market Movements",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "BlockChain Architect",
+    desc: "Designed and developed a blockchain System using Solidity and MetaMask.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
